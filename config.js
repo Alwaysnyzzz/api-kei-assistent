4e8eb134c09beb9bc262d0378daad466f13c0d00
Alwaysnyzzz <masdenys34@gmail.com>
2026-02-20 18:57:04 +0700

    ganti ke API publik

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const CONFIG = {
-    API_BASE_URL: 'http://localhost:3000', // Ganti dengan URL API online nanti
+    API_BASE_URL: 'https://brat.siputzx.my.id', // API publik
     APP_NAME: 'KEI ASSISTENT',
-    THEME_COLOR: '#ff69b4',
     WA_NUMBER: '+62 877-1347-2756',
     WA_LINK: 'https://wa.me/6287713472756',
     TELEGRAM_USERNAME: '@keikawaiii',
